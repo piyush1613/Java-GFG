6e34dbbf4ed553e9115abb9649b4d85af5e3495b
Piyush <piyushsharmapm@gmail.com>
2023-02-17 01:48:48 +0530

    second commit

--- Basic.java
+++ Basic.java
@@ -1,7 +1,7 @@
 public class Basic {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
         System.out.println("Hello World");
     }
-    
+
 }
